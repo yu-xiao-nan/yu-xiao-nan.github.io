8a8d1a7f8a473b85306dd7bafcd40e6f88c54692
faradawn <faradawny@gmail.com>
2023-06-21 23:57:22 -0500

    updated to keep up with ubuntu

--- client/globals.js
+++ client/globals.js
@@ -1,3 +1,4 @@
-const backendDomain = "http://southisland.asuscomm.com:8000/";
+const backendDomain = 'http://southisland.asuscomm.com:8000/'; // important, if local, change to '../'
+const frontendDomain = 'http://southisland.asuscomm.com:8000/client/';
 const MAX_TIME = 10;
 const SET_TIMEOUT_MS = 1000;
